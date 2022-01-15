8341f9da39036cf2440501290f7867c91974ed35
Han Dao <handao@everfit.io>
2022-01-14 23:08:37 -0600

    implemented UI for account screen

---
+++ src/Components/Account/constants.js
@@ -0,0 +1,36 @@
+export const genderOptions = [
+  { key: "m", text: "Male", value: "male" },
+  { key: "f", text: "Female", value: "female" },
+  { key: "o", text: "Other", value: "other" },
+];
+
+export const interestOptions = [
+  { key: "m", text: "Male", value: "male" },
+  { key: "f", text: "Female", value: "female" },
+  { key: "e", text: "Everyone", value: "everyone" },
+];
+
+export const keywords = [
+  { key: "Athlete", text: "Athlete", value: "Athlete" },
+  { key: "Cat Person", text: "Cat Person", value: "Cat Person" },
+  { key: "Coldplay", text: "Coldplay", value: "Coldplay" },
+  { key: "INFJ", text: "INFJ", value: "INFJ" },
+];
+
+export const cities = [
+  { key: "hcmc", text: "Ho Chi Minh City", value: "Ho Chi Minh City" },
+  { key: "hn", text: "Ha Noi", value: "Ha Noi" },
+  { key: "dn", text: "Da Nang", value: "Da Nang" },
+];
+
+export const sexualOrientaions = [
+  { key: "Straight", text: "Straight", value: "Straight" },
+  { key: "Lesbian", text: "Lesbian", value: "Lesbian" },
+  { key: "Gay", text: "Gay", value: "Gay" },
+  { key: "Bisexual", text: "Bisexual", value: "Bisexual" },
+  { key: "Pansexual", text: "Pansexual", value: "Pansexual" },
+  { key: "Asexual", text: "Asexual", value: "Asexual" },
+  { key: "Demisexual", text: "Demisexual", value: "Demisexual" },
+  { key: "Queer", text: "Queer", value: "Queer" },
+  { key: "Questioning", text: "Questioning", value: "Questioning" },
+];
